b3a5ae04b738035f1c7c83d2f717f85d10fa4b48
fiso64 <sokfilix@gmail.com>
2026-03-26 23:25:11 +0100

    fix: handler: fix incorrect path to config

--- src/handler/main.go
+++ src/handler/main.go
@@ -88,7 +88,7 @@ func getConfigDir() (string, error) {
 	if err != nil {
 		return "", err
 	}
-	return filepath.Join(userConfig, "Kinome", "handler"), nil
+	return filepath.Join(userConfig, "kinome", "handler"), nil
 }
 
 func validateSecret(providedSecret string) bool {
